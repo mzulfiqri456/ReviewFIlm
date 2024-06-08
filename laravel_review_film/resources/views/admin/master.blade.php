bc8c723465c6c3435c2946670662bc2b0483e4c7
mzulfiqri456 <mzulfiqrif@gmail.com>
2024-06-08 14:36:11 +0700

    Menambahkan Tampilan Sidebar

---
+++ laravel_review_film/resources/views/admin/master.blade.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sistem Informasi | Administrator</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{asset('/kelompok_8/plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('/kelompok_8/dist/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.2/jquery.rateyo.min.css" />
+  @stack('style')
+
+  <style>
+      .modal {
+        display: none;
+        position: fixed;
+        z-index: 1;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        background-color: rgb(0,0,0);
+        background-color: rgba(0,0,0,0.4);
+      }
+      .modal-content {
+        background-color: #fefefe;
+        margin: 15% auto;
+        left:150px;
+        padding: 20px;
+        border: 1px solid #888;
+        width: 50%;
+      }
+      .close {
+        color: #aaa;
+        float: right;
+        font-size: 28px;
+        font-weight: bold;
+      }
+      .close:hover,
+      .close:focus {
+        color: black;
+        text-decoration: none;
+        cursor: pointer;
+      }
+    </style>
+</head>
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- Navbar -->
+   @include('admin.nav')
+  <!-- /.navbar -->
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark elevation-4" style="background-color:#000000">
+    <!-- Brand Logo -->
+    <a href="/dashboard" class="brand-link text-center">
+      <span class="brand-text font-weight-light">Sistem Informasi Kritik Film</span>
+    </a>
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+            <img src="{{ asset('foto/foto_profil/' . Auth::user()->foto_profil) }}" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+            <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+        </div>
+    </div>
+      <!-- Sidebar Menu -->
+      @include('admin.side')
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>@yield('judul')</h1>
+          </div>
+          <div class="col-sm-6">
+            
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <div style="padding-left:50px;padding-right:50px;padding-bottom:50px;">
+        <section class="content">
+
+        @yield('content')
+
+        </section>
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2024 <a href="#">Kelompok 8</a>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+        <h5>Sub-Menu</h5>
+        <ul class="list-unstyled">
+            <li><a href="/review_film" data-slide="1">Halaman Pengguna</a></li>
+            <li><a href="/keluar" data-slide="2">Logout</a></li>
+        </ul>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="{{asset('/kelompok_8/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('/kelompok_8/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('/kelompok_8/dist/js/adminlte.min.js')}}"></script>
+<!-- <script src="../../dist/js/demo.js"></script> -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/rateYo/2.3.2/jquery.rateyo.min.js"></script>
+
+    <script>
+        $(function () {
+            $("#rating").rateYo({
+                rating: 0, // Nilai awal rating
+                fullStar: true, // Menggunakan bintang penuh
+                onSet: function (rating, rateYoInstance) {
+                    $("#rating_input").val(rating); // Set nilai rating pada input tersembunyi
+                }
+            });
+        });
+    </script>
+
+@stack('scripts')
+</body>
+</html>
